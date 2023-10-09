7cb21d64fe0091035384440bfa583625239caf9c
nuriashafqat <146842113+nuriashafqat@users.noreply.github.com>
2023-10-09 11:54:43 +0100

    Update median.py

--- median.py
+++ median.py
@@ -1,6 +1,14 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
-
+list = [1,2,12,7,6,3]
+list.sort()
+if (len(list) % 2 == 1):
+    median = list[len(list) // 2]
+else:
+    middle1 = list[len(list) - 1 // 2]
+    middle2 = list[len(list) + 1 //2]
+    median = (middle1 + middle2) / 2
+print(median)
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
